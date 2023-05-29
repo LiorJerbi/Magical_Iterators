616129d15d9a3b26b81db5bcba53d06cd0663a71
LiorJerbi <lior.Jerbi@msmail.Ariel.ac.il>
2023-05-29 12:27:50 +0300

    after all tests

--- Test.cpp
+++ Test.cpp
@@ -10,6 +10,7 @@ TEST_SUITE("Magical Container Methods:"){
     MagicalContainer container;
 
     TEST_CASE("add/remove/size methods"){
+        CHECK(container.size() == 0);
         CHECK_THROWS_AS(container.removeElement(2),runtime_error);  //can't remove from empty container
         container.addElement(1);
         container.addElement(7);
@@ -26,8 +27,8 @@ TEST_SUITE("Magical Container Methods:"){
 
 }
 TEST_SUITE("Iterators Methods:"){
-    MagicalContainer container;
     TEST_CASE("AscendingIterator:"){
+        MagicalContainer container;
         container.addElement(5);
         container.addElement(2);
         container.addElement(4);
@@ -36,21 +37,24 @@ TEST_SUITE("Iterators Methods:"){
         MagicalContainer::AscendingIterator asIt(container);
         auto it = asIt.begin();
         CHECK_EQ(*it,1);
+        CHECK_NOTHROW(++it); 
         auto it1 = ++it;        //should be 2
-        CHECK_NOTHROW(++it1);   
-        ++it1;
+        CHECK_EQ(*it1,2);
+        ++it1,++it;
         CHECK(it1>it);      // 5>2
         CHECK_NE(it,asIt.end());
+        
         string ans;
-        for(;it != asIt.end();++it){
-            ans += *it + ' ';
+        MagicalContainer::AscendingIterator itr =asIt.begin();
+        for(;itr != asIt.end();++itr){
+            ans += *itr + ' ';
         }
         CHECK_EQ(ans,"1 2 4 5 14 ");
+
         CHECK_NOTHROW(asIt = ++it1);
-        CHECK_EQ(asIt.begin(),it);
+        CHECK_EQ(asIt.begin(),it.begin());
         ++it1;
         CHECK(asIt.begin()<it1);    //1<14
-
         container.addElement(7);
         CHECK_EQ(6,container.size());
         asIt.setIndex(4);
@@ -60,6 +64,7 @@ TEST_SUITE("Iterators Methods:"){
         CHECK_EQ(5,container.size());
     }   
     TEST_CASE("SideCrossIterator:"){
+        MagicalContainer container;
         container.addElement(5);
         container.addElement(2);
         container.addElement(4);
@@ -69,13 +74,16 @@ TEST_SUITE("Iterators Methods:"){
         auto it = scIt.begin();
         CHECK_EQ(*it,1);
         CHECK_NOTHROW(++it);
+        auto it1 = ++it;    //should be 14
+        CHECK_EQ(*it1,14);
         string ans;
-        for(;it != scIt.end();++it){
-            ans += *it + ' ';
+        MagicalContainer::SideCrossIterator itr = scIt.begin();
+        for(;itr != scIt.end();++itr){
+            ans += *itr + ' ';
         }
         CHECK_EQ(ans,"1 14 2 5 4 ");
         CHECK_NOTHROW(scIt = ++it);
-        CHECK_EQ(scIt.begin(),it);
+        CHECK_EQ(scIt.begin(),it.begin());
 
         container.addElement(7);
         CHECK_EQ(6,container.size());
@@ -86,6 +94,7 @@ TEST_SUITE("Iterators Methods:"){
         CHECK_EQ(5,container.size());
     }
     TEST_CASE("PrimeIterator:"){
+        MagicalContainer container;
         container.addElement(5);
         container.addElement(2);
         container.addElement(4);
@@ -94,14 +103,15 @@ TEST_SUITE("Iterators Methods:"){
         MagicalContainer::PrimeIterator prIt(container);
         auto it = prIt.begin();
         CHECK_EQ(*it,2);
+        CHECK_NOTHROW(++it);
         auto it1 = ++it;        //should be 5
-        CHECK_NOTHROW(++it1);   
         CHECK(it1>it);      // 5>2
         CHECK_NE(it,prIt.end());
 
         string ans;
-        for(;it != prIt.end();++it){
-            ans += *it + ' ';
+        MagicalContainer::PrimeIterator itr = prIt.begin();
+        for(;itr != prIt.end();++itr){
+            ans += *itr + ' ';
         }
         CHECK_EQ(ans,"2 5 ");
 
